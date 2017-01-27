a139c1e880246f05215e77e400fecdb89524b29e
Joachim Nyborg <jnyborg@outlook.com>
2017-01-27 11:22:01 -0800

    Modified i3

--- install_vim.sh
+++ install_vim.sh
@@ -2,3 +2,6 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/git/dotfiles/vim/.vimrc ~/.vimrc
+mkdir ~/.config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
